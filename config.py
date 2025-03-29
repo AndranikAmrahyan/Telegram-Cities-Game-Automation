ba6ae403675008ee076d5f51019c3bfed757106e
AndranikA <andranika93@gmail.com>
2025-03-29 13:59:45 +0400

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ API_ID = int(os.getenv("API_ID"))  # Твой API ID
 API_HASH = os.getenv("API_HASH")  # Твой API HASH
 
 # Сервер на Render
-RENDER_APP_URL = "https://telegram-cities-game-automation.onrender.com"  # Ставь свою ссылку с Render. Пример: https://your_app.onrender.com
+RENDER_APP_URL = os.getenv("RENDER_APP_URL")  # Ставь свою ссылку с Render. Пример: https://your_app.onrender.com
